c3d3fc77e8f5aeba1be3f7a910f8508ba0801f1a
aditseller <tezaraditya@gmail.com>
2018-07-08 10:53:05 +0700

    REV

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -18,7 +18,26 @@ AppAsset::register($this);
     <meta charset="<?= Yii::$app->charset ?>">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-	<meta name="google-site-verification" content="XINbG_ON0K6cyuILTaTxKh-7NXwAv6pvizKAipVsU8M" />
+	 <meta name="google-site-verification" content="XINbG_ON0K6cyuILTaTxKh-7NXwAv6pvizKAipVsU8M" />
+
+   <link rel="apple-touch-icon" sizes="57x57" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-57x57.png">
+<link rel="apple-touch-icon" sizes="60x60" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-60x60.png">
+<link rel="apple-touch-icon" sizes="72x72" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-72x72.png">
+<link rel="apple-touch-icon" sizes="76x76" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-76x76.png">
+<link rel="apple-touch-icon" sizes="114x114" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-114x114.png">
+<link rel="apple-touch-icon" sizes="120x120" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-120x120.png">
+<link rel="apple-touch-icon" sizes="144x144" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-144x144.png">
+<link rel="apple-touch-icon" sizes="152x152" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-152x152.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?= Yii::$app->request->baseUrl ?>/public/ico/apple-icon-180x180.png">
+<link rel="icon" type="image/png" sizes="192x192"  href="<?= Yii::$app->request->baseUrl ?>/public/ico/android-icon-192x192.png">
+<link rel="icon" type="image/png" sizes="32x32" href="<?= Yii::$app->request->baseUrl ?>/public/ico/favicon-32x32.png">
+<link rel="icon" type="image/png" sizes="96x96" href="<?= Yii::$app->request->baseUrl ?>/public/ico/favicon-96x96.png">
+<link rel="icon" type="image/png" sizes="16x16" href="<?= Yii::$app->request->baseUrl ?>/public/ico/favicon-16x16.png">
+<link rel="manifest" href="/manifest.json">
+<meta name="msapplication-TileColor" content="#ffffff">
+<meta name="msapplication-TileImage" content="<?= Yii::$app->request->baseUrl ?>/public/ico/ms-icon-144x144.png">
+<meta name="theme-color" content="#ffffff">
+
     <?= Html::csrfMetaTags() ?>
     <title><?= Html::encode($this->title) ?></title>
     <?php $this->head() ?>
@@ -49,7 +68,7 @@ AppAsset::register($this);
 ?>
 <?php
 
-$items = app\models\Channel::find()->orderBy(['id_channel'=>SORT_DESC])->all();
+$items = app\models\Channel::find()->orderBy(['id_channel'=>SORT_ASC])->all();
 $menuItems = [];
        foreach ($items as $key => $value) {
                    $menuItems[] = ['label' => strtoupper($value['channel']), 'url' => ['/'.$value['channel']]];
